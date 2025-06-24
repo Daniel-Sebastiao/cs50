e1555df209a8046e0eaa690d82021b521595ad9c
Daniel <daniel.ds.sebastiao@gmail.com>
2025-06-24 12:58:33 -0300

    Modified: cat.py by reestructuring it to use especific functions

--- cs50p/class/cat.py
+++ cs50p/class/cat.py
@@ -1,27 +1,14 @@
-# Loops
-# Ways to execute a code repeatedly
-# Using While
-iterator = 0
-while iterator < 3:
-    print("meow")
-    iterator += 1
+def main():
+    number = get_number()
+    meow(number)
 
-# Using For att: For is usually used to loop over list, which is a data type in Python.
-for i in range(3):
-    print("meow")
+def get_number():
+    while True:
+        n = int(input("What's n "))
+        if n > 0:
+            return n
 
-# When not using a variable, do the following...
-for _ in range(3):
-    print("meow")
-
-# Using a controlled infinity loop
-while True:
-    n = int(input("How many times do you want to meow? "))
-    if n > 0:
-        break
-
-for _ in range(n):
-    print("meow")
-
-
-
+def meow(n):
+    for _ in range(n):
+        print("meow")
+main()
