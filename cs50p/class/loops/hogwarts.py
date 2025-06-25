8a9b9a84cd51864c87a21ee86e77ae9667374389
Daniel <daniel.ds.sebastiao@gmail.com>
2025-06-25 10:49:56 -0300

    New Folder Structure and Add: hogwarts.py

---
+++ cs50p/class/loops/hogwarts.py
@@ -0,0 +1,10 @@
+students = ["Hermione", "Harry", "Ron"]
+
+# Print each student
+for student in students:
+    print(student)
+
+# Print with the iterator
+for i in range(len(students)):
+    print(i+1, students[i])
+
