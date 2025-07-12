22d6a5ea14f4aa63eb769383d634179715a04670
Daniel <daniel.ds.sebastiao@gmail.com>
2025-07-12 08:21:06 -0300

    Modified number.py 'we should only try code that might raise an exception (ERROR)'

--- cs50p/class/exceptions/number.py
+++ cs50p/class/exceptions/number.py
@@ -1,8 +1,7 @@
 try:
-    # Ask an Input for the User
     x = int(input("What's X? "))
-    # If The value Typed in is an Integer
-    print(f"X is {x}")
 except ValueError:
-    # Else
-    print("X is not an Integer")
+    print("X is not an Integer") # Handling Error
+
+#
+print(f"X is {x}")
