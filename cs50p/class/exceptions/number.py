86c39b842cd643445402057e3e54104f160ba32b
Daniel <daniel.ds.sebastiao@gmail.com>
2025-07-12 08:38:08 -0300

    Modified number.py by adding functions

--- cs50p/class/exceptions/number.py
+++ cs50p/class/exceptions/number.py
@@ -1,6 +1,15 @@
-try:
-    x = int(input("What's X? "))
-except ValueError:
-    print("X is not an Integer") # Handling Error
-else:
+def main():
+    x = get_int()
     print(f"X is {x}") # Only Executes if there is not an Exception
+def get_int():
+    while True:
+        try:
+            x = int(input("What's X? "))
+        except ValueError:
+            print("X is not an Integer") # Handling Error
+        else:
+            break
+    return x
+
+main()
+
