9b082e152cbea4db52058be4a5fd88862b5bcad5
Daniel <daniel.ds.sebastiao@gmail.com>
2025-07-12 08:23:22 -0300

    Modified: number.py (Solved by executing only if there's not an exception)

--- cs50p/class/exceptions/number.py
+++ cs50p/class/exceptions/number.py
@@ -2,6 +2,5 @@ try:
     x = int(input("What's X? "))
 except ValueError:
     print("X is not an Integer") # Handling Error
-
-#
-print(f"X is {x}")
+else:
+    print(f"X is {x}") # Only Executes if there is not an Exception
