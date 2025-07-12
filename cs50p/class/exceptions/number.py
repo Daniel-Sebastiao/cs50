af740024b741bf233d55988e5b2e83268081bdcf
Daniel <daniel.ds.sebastiao@gmail.com>
2025-07-12 08:50:33 -0300

    Modified: number.py by inserting pass keyword and make the code shorter

--- cs50p/class/exceptions/number.py
+++ cs50p/class/exceptions/number.py
@@ -1,15 +1,13 @@
 def main():
     x = get_int()
     print(f"X is {x}") # Only Executes if there is not an Exception
+
 def get_int():
     while True:
         try:
-            x = int(input("What's X? "))
+            return int(input("What's X? "))
         except ValueError:
-            print("X is not an Integer") # Handling Error
-        else:
-            break
-    return x
+            pass # Handling Error
 
 main()
 
